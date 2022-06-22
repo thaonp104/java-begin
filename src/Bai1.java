12ce262ea359a67ce6443b7d2b6d85ca410dcf37
thaonp-1057 <nguyen.phuong.thao-c@sun-asterisk.com>
2022-06-22 10:30:12 +0700

    bai 1 - hello world, variable, operator

---
+++ src/Bai1.java
@@ -0,0 +1,13 @@
+public class Bai1 {
+    public static void main(String[] args) {
+        helloWorld();
+    }
+
+    private static void helloWorld() {
+        System.out.println("Hello World!");
+    }
+
+    private static void variableAndOperator() {
+
+    }
+}
